687e23e51ee95958949689b88125d5ab1f6f3036
Baileywanji <160423754+Baileywanji@users.noreply.github.com>
2024-03-17 13:23:59 +0200

    Add files via upload

---
+++ user_input.py.py
@@ -0,0 +1,22 @@
+#what the name
+#name = "Caroline" 
+#print(name)
+#The age
+#age = 33
+#print (age)
+#location
+#location = "Nairobi"
+#print (location)
+#personalised message
+#my_message = ('I am learning python programming')
+#print (my_message)
+#create message with all individual attributes
+import string
+
+name = "Caroline"
+age = 33
+city = "Nairobi"
+print(f"Hello {name}, you are {age}, living in {city}")
+
+
+
